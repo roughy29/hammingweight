5e31c3fca47d130232861aa38b53c72155f1c98d
Hayk <108818390+roughy29@users.noreply.github.com>
2023-06-13 11:23:45 +0200

    first commit

---
+++ src/main/java/com/example/hammingweight/HammingweightApplication.java
@@ -0,0 +1,13 @@
+package com.example.hammingweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingweightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HammingweightApplication.class, args);
+    }
+
+}
